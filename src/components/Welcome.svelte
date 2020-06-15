e30b199572d8a4a57234d081de06494de5651e01
sanderjson <sanderson.jma@gmail.com>
2020-06-15 15:07:31 -0600

    typo

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -21,8 +21,8 @@
   <div class="mt-8 md:mt-16">
     <!-- <TitleSmall text="Four Types" min={false} /> -->
     <div
-      class="mt-3 py-4 d:mt-5 md:py-8 lg:py-12 max-w-md mx-auto
-      md:max-w-3xl rounded-sm">
+      class="mt-3 py-4 d:mt-5 md:py-8 lg:py-12 max-w-md mx-auto md:max-w-3xl
+      rounded-sm">
       <ImageGroup links={false} />
     </div>
   </div>
@@ -30,7 +30,7 @@
   <div class="mt-8 md:mt-16">
     <TitleSmall text="About" min={false} />
     <Paragraph>
-      This is a short personality test to help you identiy your communication
+      This is a short personality test to help you identify your communication
       style. The test is based on the work of Dr. Gary Smalley and Dr. John
       Trent. This simple test may help provide insightful information into your
       personality and personal relationships.
@@ -39,8 +39,8 @@
 
   <div class="my-8 md:my-16">
     <div
-      class="mt-3 py-4 md:mt-5 md:py-8 lg:py-12 mx-auto max-w-md
-      md:max-w-3xl rounded-sm text-center">
+      class="mt-3 py-4 md:mt-5 md:py-8 lg:py-12 mx-auto max-w-md md:max-w-3xl
+      rounded-sm text-center">
       <Button onClick={takeTest} text="Take the Test" />
     </div>
   </div>
